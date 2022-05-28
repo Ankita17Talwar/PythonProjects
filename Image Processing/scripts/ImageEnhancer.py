6ecf796c5643aba5dded82da22b9ec225902c41e
Ankita17Talwar <90397833+Ankita17Talwar@users.noreply.github.com>
2022-05-28 23:41:02 +0530

    Update ImageEnhancer.py

--- Image Processing/scripts/ImageEnhancer.py
+++ Image Processing/scripts/ImageEnhancer.py
@@ -17,6 +17,15 @@ def enhance_contrast(inp_img,factor):
     new_img = img_enh_obj.enhance(factor)
     new_img.save('Output\ImageEnhance\enhanced_Bri_contrast_img.jpg')
 
+def enhance_sharpness(inp_img, factor):
+    img = Image.open(inp_img)
+    img_enh_obj = ImageEnhance.Sharpness(img)
+
+    new_img = img_enh_obj.enhance(factor)
+    new_img.save('Output\ImageEnhance\enhanced_sharp_img.png')
+
+
 if __name__ == '__main__':
     enhance_brightness('data\images\sample_img1.jpg', 2.5)
     enhance_contrast(r'Output\ImageEnhance\enhanced_brigh_img.jpg', 1.5)
+    enhance_sharpness(r'data\images\blurr_img1.png',2)
